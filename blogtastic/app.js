8f56aed0d53f2b0757ad556e3affebb2aa5d4c85
Norman <norman@Normans-MacBook.local>
2015-06-26 14:05:43 -0700

    added tic tac toe

---
+++ blogtastic/app.js
@@ -0,0 +1,20 @@
+alert("nfdnfgjk");
+
+   <div class="form-group">
+        <div class="col-sm-10 col-sm-offset-2">
+            <input id="submit" name="submit" type="submit" value="Send">
+            <input type="submit" onclick="return confirm('Are you sure?')" /
+      var button = document.querySelector("#email a");
+            class="btn btn-primary">
+
+        </div>
+       if (aboutDiv.stle.display ="none") {
+       	contact.Div.style.display
+       	else {
+       		aoutDiv.style.display = "none"
+       	}
+       }
+       submit.addEventList("click, function(event"
+       	event.preventDefault)
+       alert("")
+        
